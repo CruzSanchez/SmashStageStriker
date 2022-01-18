5a8944a68ad61d2d60452175bf4ba17661f74fd3
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-01-17 21:04:46 -0600

    Add project files.

---
+++ SmashStageStriker/SmashStageStriker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmashStageStriker.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmashStageStriker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SmashStageStriker/SmashStageStriker/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SmashStageStriker.Models;
+using SmashStageStriker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmashStageStriker.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
